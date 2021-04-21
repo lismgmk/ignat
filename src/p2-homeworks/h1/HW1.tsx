60d605b0c8d00fd02e8697dc4b7b5baeab191176
lismgmk <lismgmk2@gmail.com>
2021-04-22 02:59:12 +0300

    lesson 2

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -5,10 +5,15 @@ const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
     message: 'some text',
-    time: '22:00',
+    time: '22:00'
 };
 
-
+export type MessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 
 
 function HW1() {
@@ -19,12 +24,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import {MessageDataType} from "./HW1";
+import s from "./Message.module.css"
 
 
-
-function Message() {
+function Message(props: MessageDataType) {
     return (
-        <div>
+        <div className={s.message}>
 
+                <img src = {props.avatar} alt={'avatar'} className={s.avatarImg}/>
+
+            <div className={s.circle}></div>
+            <div className={s.text}>
+                    <div className={s.textName}>{props.name}</div>
+                    <div className={s.textContent}>{props.message}</div>
+                <div className={s.textTime}>{props.time}</div>
+            </div>
         </div>
+
     )
 }
 
